8df47eaf512157ee7f51cdca7e0e7d81af7118f5
yanchenglee98 <59784190+yanchenglee98@users.noreply.github.com>
2020-02-04 23:06:05 +0800

    Add files via upload

--- Labs/Lab 2/BigCruise.java
+++ Labs/Lab 2/BigCruise.java
@@ -1,3 +1,11 @@
-public BigCruise {
-	
+public class BigCruise extends Cruise {
+	private final int length;
+	private final int numberOfPassengers;
+
+	BigCruise(String ID, int timeOfArrival, int length, int numberOfPassengers) {
+		super(ID, timeOfArrival, (int)Math.ceil((double)length/40), (int)Math.ceil((double)numberOfPassengers/50));
+		this.length = length;
+		this.numberOfPassengers = numberOfPassengers;	
+	}
+
 }
